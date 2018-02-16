1c424abf2450c2deb175344b4ce6072e383e4d46
jonasna <jonasna93@gmail.com>
2018-02-16 10:52:27 +0100

    Added PortioningMachine - initial

---
+++ SOLID.PortioningMachine/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.PortioningMachine
+{
+    public class Class1
+    {
+        public void Swag()
+        {
+            Console.WriteLine("Dope!");
+        }
+    }
+}
